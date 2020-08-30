08f227471f553b19d6f45a01baabe5d2320ac894
Indradip Halder <indradip.halder4@gmail.com>
2020-08-30 19:47:05 +1000

    apply change in kubeCircle

--- gcloud_jenkinsx/main.tf
+++ gcloud_jenkinsx/main.tf
@@ -1,7 +1,14 @@
 module "jx" {
     source = "jenkins-x/jx/google"
     gcp_project = "jenkins-x-indradip"
-    zone = "us-west1-b"    
+    zone = "us-west1-b"
+    cluster_name = "indra-jenkins-x-cluster"
+    dev_env_approvers = "ihalder1"
+    force_destroy = true
+    git_owner_requirement_repos = "ihalder1"
+    jenkins_x_namespace = "indra-jenkins-x"
+    max_node_count = 3
+    node_disk_size = 30
 }
 
 output "jx_requirements" {
