d284847f3f52b222ebcefefef09c5a18f9389d70
Indradip Halder <indradip.halder4@gmail.com>
2020-08-30 22:46:13 +1000

    update main.tf

--- gcloud_jenkinsx/main.tf
+++ gcloud_jenkinsx/main.tf
@@ -4,9 +4,7 @@ module "jx" {
     zone = "us-west1-b"
     cluster_name = "indra-jenkins-x-cluster"
     force_destroy = true
-    jenkins_x_namespace = "indra-jenkins-x"
-    max_node_count = 3
-    node_disk_size = 30
+    
 }
 
 output "jx_requirements" {
